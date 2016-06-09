af88e1301f3fa7934c054257046f895fcf4af4bd
jayshreen <jayshreen@cybage.com>
2016-06-09 12:45:39 +0530

    changes in sprint

--- source/src/main/java/com/cyb/tms/dao/TmsSprintDAO.java
+++ source/src/main/java/com/cyb/tms/dao/TmsSprintDAO.java
@@ -2,13 +2,14 @@ package com.cyb.tms.dao;
 
 import java.util.List;
 
+import com.cyb.tms.dto.TmsSprintDTO;
 import com.cyb.tms.entity.TmsSprintMst;
 
 public interface TmsSprintDAO {
 	
-	public long createSprint(TmsSprintMst sprint);
-	public TmsSprintMst updateSprint(TmsSprintMst sprint);
-	public List<TmsSprintMst> getAllSprints();
+	public long createSprint(TmsSprintDTO tmsSprintDTO);
+	public TmsSprintMst updateSprint(TmsSprintDTO tmsSprintDTO);
+	public List<TmsSprintMst> getAllSprints(long projectId);
 	public TmsSprintMst getSprint(long id);
 	public TmsSprintMst getActiveSprint(long projectId);
 	
